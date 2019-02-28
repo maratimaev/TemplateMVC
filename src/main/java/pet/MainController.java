0a9c5788f318c73da4a449b016ef8f79eee9a877
maratimaev <mailto:imaevmarat@outlook.com>
2019-02-28 22:08:14 +0500

    Initial commit

---
+++ src/main/java/pet/MainController.java
@@ -0,0 +1,13 @@
+package pet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String view(){
+        return "index";
+    }
+}
